552ce13c267ddde9e941620075d794e0d8424087
Ankit77760 <ankitm7972@gmail.com>
2024-02-18 11:42:03 +0530

    subsequencesum

--- subsequencesum.cpp
+++ subsequencesum.cpp
@@ -11,6 +11,7 @@ void subsequence(int i, vector<int> ds, int s, int sum, int arr[], int n)
             {
                 cout << it << " ";
             }
+            cout << endl;
         }
         return;
     }
@@ -22,11 +23,42 @@ void subsequence(int i, vector<int> ds, int s, int sum, int arr[], int n)
     subsequence(i + 1, ds, s, sum, arr, n);
 }
 
+// print only once
+bool subsequencee(int i, vector<int> ds, int s, int sum, int arr[], int n)
+{
+    if (i == n)
+    {
+        // condition satisfied
+        if (sum == s)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
+            return true;
+        }
+        // condition not satisfied
+        return false;
+    }
+    ds.push_back(arr[i]);
+    s += arr[i];
+    if (subsequencee(i + 1, ds, s, sum, arr, n) == true)
+    {
+        return true;
+    }
+    s -= arr[i];
+    ds.pop_back();
+    if (subsequencee(i + 1, ds, s, sum, arr, n) == true)
+    {
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
     int arr[3] = {1, 2, 1};
     int n = 3;
     int sum = 2;
     vector<int> ds;
-    subsequence(0, ds, 0, sum, arr, n);
+    subsequencee(0, ds, 0, sum, arr, n);
 }
