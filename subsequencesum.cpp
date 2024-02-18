f09a4231a393d1620c6f460fcc3d5566e034a49a
Ankit77760 <ankitm7972@gmail.com>
2024-02-18 12:03:41 +0530

    subsequencesum

--- subsequencesum.cpp
+++ subsequencesum.cpp
@@ -54,11 +54,38 @@ bool subsequencee(int i, vector<int> ds, int s, int sum, int arr[], int n)
     return false;
 }
 
+// count of subsequence with sum k
+int subsequenceee(int i, vector<int> ds, int s, int sum, int arr[], int n)
+{
+    if (i == n)
+    {
+        // condition satisfied
+        if (sum == s)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
+            return 1;
+        }
+        // condition not satisfied
+        return 0;
+    }
+    ds.push_back(arr[i]);
+    s += arr[i];
+
+    int left = subsequencee(i + 1, ds, s, sum, arr, n);
+    s -= arr[i];
+    ds.pop_back();
+    int right = subsequencee(i + 1, ds, s, sum, arr, n);
+    cout << "Total Number of Subsequence are: ";
+    return left + right;
+}
+
 int main()
 {
     int arr[3] = {1, 2, 1};
     int n = 3;
     int sum = 2;
     vector<int> ds;
-    subsequencee(0, ds, 0, sum, arr, n);
+    cout << subsequenceee(0, ds, 0, sum, arr, n);
 }
