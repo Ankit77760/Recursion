38d4a667c911fa449bb7c328600f4b23de053603
Ankit77760 <ankitm7972@gmail.com>
2024-07-12 11:32:54 +0530

    multiple occurences linear search

--- Array_Recursion/linear_search.cpp
+++ Array_Recursion/linear_search.cpp
@@ -11,7 +11,25 @@ int searchh(int arr[],int index,int target,int size){
     searchh(arr,index+1,target,size);
 }
 
+vector<int> searchmultiple(int arr[],int index,int size,int target,vector<int>&ans){
+    if(index==size){
+        return ans;
+    }
+    if(arr[index]==target){
+        ans.push_back(index);
+    }
+    return searchmultiple(arr,index+1,size,target,ans);
+}
+
 int main(){
     int arr[5]={1,2,3,4,5};
     cout<<searchh(arr,0,2,5);
+
+    cout<<"multiple occurences";
+    vector<int>ans;
+    int arrr[9]={1,2,3,8,5,6,8,8,8};
+    searchmultiple(arrr,0,9,8,ans);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
 }
